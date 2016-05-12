81642bea026e52f7c7a5e3e9bec2aafb551669db
David Newbury <david.newbury@gmail.com>
2016-05-11 23:38:42 -0400

    Minor cleanups, improved code legibility

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,10 +1,17 @@
 module Sinatra
   module TerrapatternHelpers
 
+    # Handle redirecting to the www subdomain if not already there
     def send_to_www
       redirect request.url.sub(/:\/\/(?:\w+?\.)/, '://www.'), 301 unless request.host =~ /^www/
       return nil
     end
+
+    # Wrap the markdown helper.  Kludge for not being a real Sinatra extension.
+    def insert_content(id)
+      $markdown.insert_content(id)
+    end
+
   end
   helpers TerrapatternHelpers
 end
--- terrapattern.rb
+++ terrapattern.rb
@@ -1,4 +1,4 @@
-# ------------------- Loading Libraries ---------------------------------------
+# Loading Libraries 
 require 'sinatra/base'
 require "sinatra/reloader" 
 require "sinatra/json"
@@ -28,12 +28,18 @@ require_relative "lib/helpers"
 class Terrapattern < Sinatra::Base
   # enable :sessions
   # set :session_secret, ENV["SESSION_SECRET"]
+
+  # Load the data from the cities config file
   set :city_data, YAML::load(File.open('data/cities.yaml'))["cities"]
   set :city_urls, settings.city_data.collect{|city| city["url_name"]}
   set :city_names, settings.city_data.collect{|city| city["name"]}
+
+  # initialize some of the helper classes
+  # TODO: Convert these to proper Sinatra Extensions. (Low priority—they work.)
   $tile_lookup = TileLookup.new
   $markdown = MarkdownPartial.new
 
+  # Register the various extensions and helpers
   register Sinatra::Flash
   register Sinatra::Namespace
   register Sinatra::Partial
@@ -41,39 +47,38 @@ class Terrapattern < Sinatra::Base
 
   helpers Sinatra::JSON
   helpers Sinatra::ContentFor
-
   helpers Sinatra::TerrapatternHelpers
-  helpers do 
-    def insert_content(id)
-      $markdown.insert_content(id)
-    end
-  end
  
+  # Set up some specific functionality for the development environment
   configure :development do
     register Sinatra::Reloader
      also_reload 'lib/markdown_partial.rb' 
      also_reload 'lib/helpers.rb' 
   end
 
+  ##----
+  # BELOW HERE ARE ROUTES
+  ##----
+
+
+  # Handle the landing page
   subdomain :www do
     get '/' do
       haml :index
     end
-
-
   end
  
   subdomain do
 
-    get '/images/*.*' do |file,ext|
-      path = "content/images/#{file}.#{ext}"
-      unless File.exist? path
-        pass
-      end
-      content_type ext.to_sym
-      File.read(path, mode: "rb")
+    # The actual Terrapattern interface
+    get "/" do
+      send_to_www unless settings.city_urls.include? subdomain
+      @city_data = settings.city_data.find{|city| city["url_name"] == subdomain.to_s}
+      @geojson = File.read(@city_data["geojson"])
+      haml :interface
     end
 
+    # Static pages.  Note that they will redirect to the www subdomain.
     get "/team" do
       send_to_www
       haml :team
@@ -89,17 +94,21 @@ class Terrapattern < Sinatra::Base
       haml :faq
     end
 
-    get "/" do
-      send_to_www unless settings.city_urls.include? subdomain
-      @city_data = settings.city_data.find{|city| city["url_name"] == subdomain.to_s}
-      @geojson = File.read(@city_data["geojson"])
-      haml :interface
-    end
-
+    # JSON route for connecting to the search implementation
     get "/search" do
       redirect("/") unless settings.city_urls.include? subdomain
       city_data = settings.city_data.find{|city| city["url_name"] == subdomain.to_s}
       json $tile_lookup.lookup(params['lat'], params['lng'], city_data["search_locale"], 19)
+    end
+
+    # Special helper for the Markdown images.
+    get '/images/*.*' do |file,ext|
+      path = "content/images/#{file}.#{ext}"
+      unless File.exist? path
+        pass
+      end
+      content_type ext.to_sym
+      File.read(path, mode: "rb")
     end
 
   end
